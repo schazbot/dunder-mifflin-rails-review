7b2dc0547f26fedae02f304777112d57514cab24
github username <github email address>
2019-03-20 16:53:18 +0000

    Added sort button

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,14 +1,20 @@
 class DogsController < ApplicationController
 
   def index
-    @dogs = Dog.all.sort_by{|dog| dog.employees.count}.reverse
+    if flash[:sort]
+      @dogs = Dog.all.sort_by{|dog| dog.employees.count}.reverse
+    else
+      @dogs = Dog.all
+    end
   end
 
   def show
     @dog = Dog.find(params[:id])
   end
 
-
-
+  def sort
+    flash[:sort] = true
+    redirect_to dogs_path
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :dogs
   resources :employees
+
+  post "dogs/sort", to: "dogs#sort", as: "sort"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
